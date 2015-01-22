144695369575aad9c7c31cfc39ef926ba3de1aed
wwb <wwb@itcast.cn>
2015-01-22 19:30:27 +0800

    tijiao

---
+++ src/com/example/androidproject/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.androidproject;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		//这是我的第一个版本   我在第一个版本后面添加了一句话
+		//我们继续开发第二个版本不
+	}
+
+	
+
+}
